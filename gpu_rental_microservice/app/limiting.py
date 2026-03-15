65b66a57fed56eb37a712bc604628d72f9e4e59e
cosDeptInnova <ia.cos.gdc@outlook.com>
2026-03-15 17:56:52 +0100

    Delete gpu_rental_microservice directory

--- gpu_rental_microservice/app/limiting.py
+++
@@ -1,9 +0,0 @@
-from .redis_control import allow_rate_limit
-
-
-class RedisSlidingWindowRateLimiter:
-    def allow(self, key: str, limit: int, window_seconds: int = 60) -> bool:
-        return allow_rate_limit(key=key, limit=limit, window_seconds=window_seconds)
-
-
-rate_limiter = RedisSlidingWindowRateLimiter()
